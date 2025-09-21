10e65fa4f7024aa367b85fd5d721393f73c35993
Overdr0ne <scmorris.dev@gmail.com>
2025-09-20 18:58:45 -0600

    add dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,14 @@
+(
+ (nil . (
+         (eval . (let ((project-root-path (expand-file-name "~/.emacs.d/overdr0ne")))
+                   (setq-local remember-data-file (concat project-root-path "notes/mem.md"))
+                   (setq-local remember-annotation nil)
+                   (setq-local remember-data-directory (concat project-root-path "notes/"))
+                   (setq-local remember-initial-contents nil)
+                   (setq-local remember-notes-initial-major-mode 'markdown-mode)
+                   (setq-local remember-notes-buffer-name (concat "*Remember:" project-list-file "*"))
+                   (setq-local sam-root-directory "~/.emacs.d/overdr0ne/")
+                   (setq-local sam-notes-directory "~/.emacs.d/overdr0ne/notes/")
+                   ))
+         ))
+ )
